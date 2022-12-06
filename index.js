900d89abbac49444d7634eb97f5dbd49126898d9
RennieNjuguna <rennynjuguna@gmail.com>
2022-12-06 21:37:21 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector('main#main').remove();
+let newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'MITCHELL is the champion'
+newHeader.className = 'victory';
