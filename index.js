b9575d35b287e769522539758be66c63db0e5496
rennynjuguna <118157651+rennynjuguna@users.noreply.github.com>
2022-12-07 15:56:38 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 document.querySelector('main#main').remove();
 let newHeader = document.createElement('h1');
 newHeader.id = 'victory';
-newHeader.textContent = 'MITCHELL is the champion'
+newHeader.textContent = 'RENNY is the champion'
 newHeader.className = 'victory';
